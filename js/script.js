d75fc30918665196626359e32ebd5056b57358a4
magic404 <jamesbox3@gmail.com>
2023-01-05 16:46:41 -0800

    added dummy questions to js

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var questions = [
+    {
+        question: "Q1",
+        ansChoices:  ["A1", "A2", "A3", "A4"],
+        corrAns: "A3",
+    },
+    {
+        question: "Q2",
+        ansChoices:  ["A1", "A2", "A3", "A4"],
+        corrAns: "A1",
+    },
+    {
+        question: "Q3",
+        ansChoices:  ["A1", "A2", "A3", "A4"],
+        corrAns: "A2",
+    },
+    {
+        question: "Q4",
+        ansChoices:  ["A1", "A2", "A3", "A4"],
+        corrAns: "A3",
+    },
+    {
+        question: "Q5",
+        ansChoices:  ["A1", "A2", "A3", "A4"],
+        corrAns: "A4",
+    },
+    {
+        question: "Q6",
+        ansChoices:  ["A1", "A2", "A3", "A4"],
+        corrAns: "A1",
+    },
+
+];
