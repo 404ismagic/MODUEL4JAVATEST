aedda23b3213047dd2a18b662ae7476e8fd7211e
magic404 <jamesbox3@gmail.com>
2023-01-10 17:32:07 -0800

    Java Multiple Choice

--- js/script.js
+++ js/script.js
@@ -1,38 +1,42 @@
 var questions = [
     {
-        question: "Q1",
-        ansChoices:  ["A1", "A2", "A3", "A4"],
-        corrAns: "A3",
+        question: "Javascript is an _______ language?",
+        ansChoices:  ["Object-Orinted", "Object-Based", "Procedural", "None of the above"],
+        corrAns: "Object-Oriented",
     },
     {
-        question: "Q2",
-        ansChoices:  ["A1", "A2", "A3", "A4"],
-        corrAns: "A1",
+        question: "Which of the Folloiwng key words is used to define a variable in Javascript",
+        ansChoices:  ["var", "let", "Both A and B", "None of the above"],
+        corrAns: "Both A and B",
     },
     {
-        question: "Q3",
-        ansChoices:  ["A1", "A2", "A3", "A4"],
-        corrAns: "A2",
+        question: "Which of the following methods is used to access HTML elemnts using Javascript?",
+        ansChoices:  ["getElementbyid()", "getElementByClassName()", "Both A and B", "None of the above"],
+        corrAns: "Both A and B",
     },
     {
-        question: "Q4",
-        ansChoices:  ["A1", "A2", "A3", "A4"],
-        corrAns: "A3",
+        question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+        ansChoices:  ["Throws an error", "Ignored the statements", "Give a warning", "None of the above"],
+        corrAns: "Ignored the statements",
     },
     {
-        question: "Q5",
-        ansChoices:  ["A1", "A2", "A3", "A4"],
-        corrAns: "A4",
+        question: "Which of the following methods can be used to display data in some form using Javascript?",
+        ansChoices:  ["document.write()", "console.log()", "window.alert()", "None of the above"],
+        corrAns: "All of the above",
     },
     {
-        question: "Q6",
-        ansChoices:  ["A1", "A2", "A3", "A4"],
-        corrAns: "A1",
+        question: "How can a datatype be declared to be a constant Type?",
+        ansChoices:  ["const", "var", "let", "constant"],
+        corrAns: "const",
     },
 
 ];
+var questionIndex = 0;
+var currentQuestion;
 var startDiv = document.getElementById("start-div");
 var questionsDiv  = document.getElementById("questions-div");
+var choicesDiv  = document.getElementById("choices-div");
+var questionEl = document.getElementById("question");
 var timerId;
 var timeEl = document.getElementById("time");
 var timeLeft = 120;
@@ -48,6 +52,23 @@ function startQuiz() {
 }
 function fetchQuestion() {
     //tbd
+    currentQuestion = questions[questionIndex];
+    questionEl.textContent = currentQuestion.question;
+
+    choicesDiv.innerHTML = '';
+
+    for ( let i =0; i < currentQuestion.ansChoices.length; i++) {
+        let choice = currentQuestion.ansChoices[i];
+        let choiceEl = document.createElement('button');
+        choiceEl.setAttribute ('class', 'choice');
+        choiceEl.setAttribute('value', choice);
+
+        choiceEl.textContent = i + 1 + " . " + choice;
+
+        choicesDiv.appendChild(choiceEl);
+    }
+
+
 }
 function oneSecHandler() {
     timeLeft--;
